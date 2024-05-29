263595ade1eb413e887e81253e065f7ab698a689
Jayanth <jayanthambaldhage15@gmail.com>
2024-05-29 11:38:24 +0530

    Initial Commit

---
+++ iOS Concurrency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Concurrency
+//
+//  Created by Jayanth Ambaldhage on 29/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Concurrency/iOS_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ConcurrencyApp.swift
+//  iOS Concurrency
+//
+//  Created by Jayanth Ambaldhage on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
